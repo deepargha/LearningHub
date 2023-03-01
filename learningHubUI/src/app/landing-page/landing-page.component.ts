4e60ee0edef603c9a5bcc87d6a6c4eec75d33e3a
smukherjee <arghadeepa9495@gmail.com>
2023-03-02 00:10:38 +0530

    UI validation to save and check user data

--- learningHubUI/src/app/landing-page/landing-page.component.ts
+++ learningHubUI/src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl, NgForm } from '@angular/forms';
+import { User } from 'src/user';
 
 
 @Component({
@@ -10,6 +11,11 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
 export class LandingPageComponent implements OnInit {
   registrationForm!: FormGroup;
   start: boolean = false
+  user:User = new User();
+  usersList: any[] = [];
+  errorMsg:string | undefined;
+  user2:User = new User();
+
   constructor(private formBuilder: FormBuilder) { 
     this.registrationForm = this.formBuilder.group({
       name: ['', Validators.required],
@@ -22,8 +28,33 @@ export class LandingPageComponent implements OnInit {
  
 
   ngOnInit(): void {
-    setTimeout(() => this.start = true, 1000)
+    setTimeout(() => this.start = true, 1000);
+    this.user2.name="abc";
+    this.user2.email="abc@gmail.com";
+    this.user2.password="12345678";
+    this.usersList.push(this.user2);
+    for(const user of this.usersList){
+      console.log(user);
+    }
+  }
 
+  saveUser(){
+    
+   /* if(this.user.email=="s.mukherjee@yahoo.com"){
+      this.errorMsg='This email already exists!';
+    }*/
+    
+    for(const email of this.usersList){
+      console.log(email);
+      if(email.email==this.user.email){
+        this.errorMsg='This email already exists!';
+      }
+      else{
+        this.usersList.push(this.user);
+      }
+    }
+    
+    console.log(this.user);
   }
 
 }
